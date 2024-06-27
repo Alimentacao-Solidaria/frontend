3330d52e814af880d7bfb156c5d9ad06c2687c3b
Thilliê Cristina Moreira de Souza <thillie.moreira@gmail.com>
2024-06-27 12:42:15 -0300

    Sobre nós: alimentação solidária

---
+++ src/paginas/sobre_nos/SobreNos.tsx
@@ -0,0 +1,49 @@
+import './style.css';
+
+function SobreNos(){
+    return (
+        <section>
+
+            <div className="background"></div>
+            <h1>AlimentAção Solidária: comida, amor & vida</h1>
+            <br />
+            <div className="container">
+                    <p className="objetivo">
+                        AlimentAção Solidária, sua plataforma de doações dedicada a combater a vulnerabilidade 
+                        alimentar infantil e a promover a saúde e o bem-estar das crianças em Roraima. Juntos, podemos acabar com as mortes 
+                        evitáveis de recém-nascidos e crianças menores de 5 anos.
+                    </p>
+                    <br />
+                <div className="paragrafo2">
+                <p>Nossa missão: Até 2030, queremos acabar com as mortes evitáveis de recém-nascidos e crianças menores de 5 anos 
+                    em Roraima, reduzindo a mortalidade neonatal para pelo menos 12 por 1.000 nascidos vivos e a mortalidade infantil 
+                    para pelo menos 25 por 1.000 nascidos vivos.</p>
+                    <br />
+                    <p>Nosso Programa: Cesta Básica Nutritiva: Fornece alimentos básicos e nutritivos para uma família por um mês.
+                        Kit de Suplementos Infantis: Inclui suplementos nutricionais essenciais para crianças menores de 5 anos.
+                        Pacote de Educação Nutricional: Apoia programas de educação nutricional em comunidades vulneráveis.</p>
+                        <br />
+                        <p>Idealizadores do projeto:
+                            <br />
+                            DANIELLA GOMES CORREA RAMOS
+                            <br />
+                            GABRIEL REZENDE
+                            <br />
+                            GIOVANA RODRIGUES SARAIVA
+                            <br />
+                            LUCAS VINICIUS GOMES
+                            <br />
+                            MAYARA ROCHA
+                            <br />
+                            THILLIÊ MOREIRA 
+                            <br />
+                            UEBER JAMES NASCIMENTO
+        </p>
+        </div>
+            </div>
+        </section>
+    
+    )
+}
+
+export default SobreNos;
