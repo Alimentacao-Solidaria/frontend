3cfd4d00b45150e8c6101ee163bb99defa348005
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-07-15 18:34:45 -0300

    task: atualizando o carrossel

--- src/components/carrossel/Carrossel.tsx
+++ src/components/carrossel/Carrossel.tsx
@@ -30,34 +30,24 @@ function CustomCarousel() {
           />
         </SwiperSlide>
         <SwiperSlide>
-          <div className="bg-red-600 h-full flex items-center justify-center text-white">
-            <div className="p-6 text-center">
-              <span className="text-3xl font-sans">
-                15.1 POR 1.000 NASCIDOS VIVOS FOI A TAXA DE MORTALIDADE INFANTIL EM RORAIMA EM 2019​​.
-              </span>
-              <h6 className="text-sm mt-2">*World Bank Open Data</h6>
-            </div>
-          </div>
+        <img
+            src="../../../src/assets/carrossel2.jpeg"
+            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            alt="Slide 2"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            src="../../../src/assets/test2.jpeg"
+        <img
+            src="../../../src/assets/carrossel3.jpeg"
             className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
             alt="Slide 3"
           />
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            src="../../../src/assets/test2.jpeg"
+        <img
+            src="../../../src/assets/carrossel4.jpeg"
             className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
             alt="Slide 4"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="../../../src/assets/test2.jpeg"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="Slide 5"
           />
         </SwiperSlide>
       </Swiper>
